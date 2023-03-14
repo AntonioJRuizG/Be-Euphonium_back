fd576275ac0e53af45324f24e4ea9c580e1e23fb
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-14 11:30:53 +0100

    Fix user mongo model parameter name

--- src/repository/user.mongo.model.ts
+++ src/repository/user.mongo.model.ts
@@ -23,7 +23,7 @@ userSchema.set('toJSON', {
     returnedObject.id = returnedObject._id;
     delete returnedObject.__v;
     delete returnedObject._id;
-    delete returnedObject.password;
+    delete returnedObject.pw;
   },
 });
 
