7a1b27199af0df45bd2ec34f8df9bd9eb1bbe5a5
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-20 12:26:14 +0100

    Update users repo test

--- src/repository/users.mongo.repo.test.ts
+++ src/repository/users.mongo.repo.test.ts
@@ -36,4 +36,50 @@ describe('Given UsersMongoRepo', () => {
       expect(result).toEqual({ email: 'test-email' });
     });
   });
+
+  describe('When the search method is used', () => {
+    test('Then the search method should be called', async () => {
+      (UserModel.find as jest.Mock).mockResolvedValue({ email: 'test-email' });
+      const result = await repo.search({ key: 'test', value: 'test-email' });
+      expect(UserModel.find).toHaveBeenCalled();
+      expect(result).toEqual({ email: 'test-email' });
+    });
+  });
+
+  describe('When the queryId method is used', () => {
+    test('Then the findbyid method should be called', async () => {
+      (UserModel.findById as jest.Mock).mockResolvedValue({
+        name: 'test',
+        email: 'test-email',
+      });
+      const result = await repo.queryId('1');
+      expect(UserModel.findById).toHaveBeenCalled();
+      expect(result).toEqual({ name: 'test', email: 'test-email' });
+    });
+
+    test('Then it should throw error if there is no data', async () => {
+      (UserModel.findById as jest.Mock).mockResolvedValue('');
+      expect(async () => repo.queryId('')).rejects.toThrow();
+    });
+  });
+
+  describe('When the update method is used', () => {
+    test('Then the findByIdAndUpdate method should be called', async () => {
+      (UserModel.findByIdAndUpdate as jest.Mock).mockResolvedValue({
+        name: 'test-updated',
+        email: 'test-email',
+      });
+      const result = await repo.update({
+        name: 'test',
+        email: 'test-email',
+      });
+      expect(UserModel.findByIdAndUpdate).toHaveBeenCalled();
+      expect(result).toEqual({ name: 'test-updated', email: 'test-email' });
+    });
+
+    test('Then it should throw error if there is no data', async () => {
+      (UserModel.findByIdAndUpdate as jest.Mock).mockResolvedValue(undefined);
+      expect(async () => repo.update({ email: 'test' })).rejects.toThrow();
+    });
+  });
 });
