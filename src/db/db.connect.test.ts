75c33e38141b142061d249c44ce007b38c2939c5
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-28 13:24:01 +0200

    Test dbconnect complete coverage

--- src/db/db.connect.test.ts
+++ src/db/db.connect.test.ts
@@ -4,13 +4,28 @@ import { dbConnect } from './db.connetc';
 
 jest.mock('mongoose');
 
-describe('dbConnect', () => {
-  it('should call mongoose.connect with the correct URI', async () => {
-    const { user, password, cluster, dbName } = config;
-    const uri = `mongodb+srv://${user}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
+describe('Given dbConnect', () => {
+  const { user, password, cluster, dbName } = config;
+  describe('Given dbConnect', () => {
+    test('Then should call mongoose.connect with the correct URI', async () => {
+      const uri = `mongodb+srv://${user}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
 
-    await dbConnect();
+      await dbConnect();
 
-    expect(mongoose.connect).toHaveBeenCalledWith(uri);
+      expect(mongoose.connect).toHaveBeenCalledWith(uri);
+      mongoose.disconnect();
+    });
+
+    test('Then should call mongoose.connect with the correct testing URI', async () => {
+      process.env.NODE_ENV = 'Test';
+      const dbTest = dbName + '-Testing';
+      const uritest = `mongodb+srv://${user}:${password}@${cluster}/${dbTest}?retryWrites=true&w=majority`;
+      await dbConnect();
+
+      const result = await dbConnect();
+      expect(typeof result).toBe(typeof mongoose);
+      expect(mongoose.connect).toHaveBeenCalledWith(uritest);
+      mongoose.disconnect();
+    });
   });
 });
